5354b1a8551912c90282b8d21088e059428d06b8
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-10-23 22:04:11 +0300

    Ex1 finished

--- ex1.c
+++ ex1.c
@@ -1,4 +1,18 @@
 #include<stdio.h>
-//hello world
-int main(void);
-printf("Aylin");
+
+int main(void){
+
+int guess;
+int number = 3;
+
+printf("Guess a number between 1 and 5: ");
+scanf("%d", &guess);
+
+if (guess == number)
+    {
+    printf("Correct!");
+    }
+else   {
+    printf("Wrong guess! Try again.");
+       }
+}
