6305171b7a4cc14662df485e858365afeb0ffc2c
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-10-26 13:59:56 +0300

    Ex2finished

--- ex2.c
+++ ex2.c
@@ -1,1 +1,14 @@
-//hello world
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("What is your age? ");
+    scanf("%d", &age);
+
+    
+    for (int i = 0; i < age; i++) {
+        printf("Happy Birthday! \n" );
+    }
+  
+}
