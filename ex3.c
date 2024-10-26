b7034f21af0a458c069ba74c9fb65a9dda5a7f5d
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-10-26 14:49:55 +0300

    Exc3done

--- ex3.c
+++ ex3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    char pet[10];
+
+    printf("Do you have a dog or a cat? ");
+    scanf("%s", pet);
+
+    if (strcmp(pet, "cat") == 0) {
+        printf("Meow! This is how a cat sounds like.\n");
+    } else if (strcmp(pet, "dog") == 0) {
+        printf("Woof! This is how a dog sounds like.\n");
+    } else if (strcmp(pet, "none") ==0) {
+        printf("You should adopt one!\n");
+    }
+  return 0;
+
+}
